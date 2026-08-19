50f9d46fdd9a8a8c23b085140de883282352510d
Gabriel Orofino <g.orofino@protonmail.com>
2026-08-19 00:50:10 -0300

    feat: add Simples Nacional annex tax tables

---
+++ data/index.js
@@ -0,0 +1,13 @@
+const anexo1 = require('./anexo1.json')
+const anexo2 = require('./anexo2.json')
+const anexo3 = require('./anexo3.json')
+const anexo4 = require('./anexo4.json')
+const anexo5 = require('./anexo5.json')
+
+module.exports = {
+    anexo1,
+    anexo2,
+    anexo3,
+    anexo4,
+    anexo5,
+}
